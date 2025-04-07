f1e0fa5abe8d11d6f0669de9eac2b5c74b2703be
ARUNBA13U <133482396+ARUNBA13U@users.noreply.github.com>
2025-04-07 21:52:29 +0530

    Update greetingWorld.groovy

--- vars/greetingWorld.groovy
+++ vars/greetingWorld.groovy
@@ -1,3 +1,3 @@
 def call() {
-    "Greeting from DevOps Team, thanks for triggering the build. Have a good day!"
+     sh "Greeting from DevOps Team, thanks for triggering the build. Have a good day!"
 }
